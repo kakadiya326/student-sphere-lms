0d2f41fca3c15afb53bfb5a972651ea374cc8772
kakadiya326 <kakadiyachiranj044@gmail.com>
2026-03-21 23:41:06 +0530

    otp sending api and email verification middleware

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,12 +1,14 @@
 let express = require('express')
 const { register, login } = require('../controllers/userController')
+const verifyOTP = require('../middleware/verifyMail');
+const sendOTP = require('../middleware/sendOTP');
 let router  = express.Router()
 
 // const checkRole = require('../middleware/checkRole')
 
 // userRoute.post('/create-course', verifyToken, checkRole("teacher"), handler)
-
-router.post('/register', register)
+router.post('/sendotp', sendOTP);
+router.post('/register', verifyOTP, register)
 router.post('/login', login)
 
 module.exports = router
